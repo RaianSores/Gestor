e323494e8e6423a38038b90755867f8d3f3288d8
unknown <rhayambejhamim@gmail.com>
2020-11-07 09:47:17 -0300

    :checkered_flag: release 07/11/2020

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const connection = require('../database/connection')
-const express = require('express')
-const router = express.Router()
-const TaskController = require('../controllers/TaskController')
-
-router.get('/clientes', TaskController.listaClientes)
-
-module.exports = router;
